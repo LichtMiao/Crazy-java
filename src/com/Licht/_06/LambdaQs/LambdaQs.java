5cf4a4851eef84d533ccfc7bb3fccb5ae43f638a
LichtMiao <2804545567@qq.com>
2018-03-07 21:42:04 +0800

    Lambda的常用形式

--- src/com/Licht/_06/LambdaQs/LambdaQs.java
+++ src/com/Licht/_06/LambdaQs/LambdaQs.java
@@ -14,11 +14,16 @@ public class LambdaQs {
 	}
 	public static void main(String[] args){
 		LambdaQs lq = new LambdaQs();
-		lq.eat(() -> System.out.println("这个苹果的味道不错"));
+		//Lambda表达式不带形参的匿名方法
+		lq.eat(() -> {
+			System.out.println("这个苹果的味道不错");
+		});
+		//Lambda表达式带一个形参，形参外的括号可以省略
 		lq.drive(weather -> {
 			System.out.println("今天天气是" + weather);
 			System.out.println("直升机飞行平稳");
 		});
+		//Lambda表达式带两个形参，返回值前的return语句可省略，方法体只有一句，则大括号可省略
 		lq.test((a, b) -> a + b);
 	}
 }
