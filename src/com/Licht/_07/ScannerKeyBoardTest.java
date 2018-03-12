da95871cf77d0727fa7105373840daf25641e6ab
LichtMiao <2804545567@qq.com>
2018-03-12 22:22:46 +0800

    使用Scanner类获取键盘输入

---
+++ src/com/Licht/_07/ScannerKeyBoardTest.java
@@ -0,0 +1,15 @@
+package com.Licht._07;
+
+import java.util.Scanner;
+
+public class ScannerKeyBoardTest {
+	public static void main(String[] args){
+		//System.in标准输入，即键盘输入
+		Scanner sc = new Scanner(System.in);
+		//判断是否还有下一个输入项
+		while(sc.hasNext()){
+			System.out.println("键盘输入的内容是："
+					+ sc.next());
+		}
+	}
+}
