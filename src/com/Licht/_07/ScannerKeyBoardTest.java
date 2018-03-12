42fee0b8f519c3f7d3869e285cf9a3da79e5b8ee
LichtMiao <2804545567@qq.com>
2018-03-12 22:29:37 +0800

    useDelimiter()方法可设置将回车符作为分隔符

--- src/com/Licht/_07/ScannerKeyBoardTest.java
+++ src/com/Licht/_07/ScannerKeyBoardTest.java
@@ -6,6 +6,8 @@ public class ScannerKeyBoardTest {
 	public static void main(String[] args){
 		//System.in标准输入，即键盘输入
 		Scanner sc = new Scanner(System.in);
+		//增加下面一行，将只把回车作为分隔符
+		sc.useDelimiter("\n");
 		//判断是否还有下一个输入项
 		while(sc.hasNext()){
 			System.out.println("键盘输入的内容是："
