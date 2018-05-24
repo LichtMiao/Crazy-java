7c33bcfcec2f9d025feaf98b8d0d1cabf85ec3d9
LichtMiao <2804545567@qq.com>
2018-05-24 21:36:03 +0800

    Runtime类exec()方法来启动操作系统的进程

---
+++ src/com/Licht/_07/ExecTest.java
@@ -0,0 +1,12 @@
+package com.Licht._07;
+
+public class ExecTest {
+	public static void main(String[] args) throws Exception
+	{
+		Runtime rt = Runtime.getRuntime();
+		//运行记事本程序
+		rt.exec("notepad.exe"); //不能直接以这种方式运行计算机非自带程序
+		//运行画图 
+	    rt.exec("mspaint.exe");
+	}
+}
