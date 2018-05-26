af00ddf895ac4a79d2f964b4f9611258c58d6eb0
LichtMiao <2804545567@qq.com>
2018-05-26 21:11:15 +0800

    Runtime类的exec()方法可直接运行计算机子带的程序

--- src/com/Licht/_07/ExecTest.java
+++ src/com/Licht/_07/ExecTest.java
@@ -6,7 +6,7 @@ public class ExecTest {
 		Runtime rt = Runtime.getRuntime();
 		//运行记事本程序
 		rt.exec("notepad.exe"); //不能直接以这种方式运行计算机非自带程序
-		//运行画图 
+		//运行画图程序 
 	    rt.exec("mspaint.exe");
 	}
 }
