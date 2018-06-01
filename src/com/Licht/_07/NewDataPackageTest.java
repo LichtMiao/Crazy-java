187ce9241d133b02fd2b323efb7803de959a5a63
LichtMiao <2804545567@qq.com>
2018-06-02 02:04:42 +0800

    java.time包下的一些常用类用法

--- src/com/Licht/_07/NewDataPackageTest.java
+++ src/com/Licht/_07/NewDataPackageTest.java
@@ -1,4 +1,7 @@
-import java.time
+package com.Licht._07;
+
+import java.time.*;
+
 public class NewDataPackageTest{
 	public static void main(String[] args){
 	/***clock的用法***/
@@ -8,7 +11,7 @@ public class NewDataPackageTest{
 	System.out.println(clock.millis());
 	System.out.println(System.currentTimeMillis());
 	/***Duration的用法***/
-	Duration d = Duration.ofSecond(6000);
+	Duration d = Duration.ofSeconds(6000);
 	System.out.println("6000ms相当于" + d.toMinutes() + "分");
 	System.out.println("6000ms相当于" + d.toHours() + "小时");
 	System.out.println("6000ms相当于" + d.toDays() + "天");
@@ -19,13 +22,13 @@ public class NewDataPackageTest{
 	Instant instant = Instant.now();
 	System.out.println(instant);
 	//在instant基础上增加6000ms，返回新的instant值
-	Instant instant2 = Instant.plusseconds(6000);
+	Instant instant2 = instant.plusSeconds(6000);
 	System.out.println(instant2);
 	//根据字符串解析instant对象
 	Instant instant3 = Instant.parse("2014-02-23T10:12:35.342Z");
 	System.out.println(instant3);
 	//在instant3基础上增加5小时4分，返回新的instant4值
-	Instant instant4 = instant3.plus(Duration.ofHours(5).pluseMinutes(4));
+	Instant instant4 = instant3.plus(Duration.ofHours(5).plusMinutes(4));
 	System.out.println(instant4);
 	//获取instant4 5天以前的时刻
 	Instant instant5 = instant4.minus(Duration.ofDays(5));
