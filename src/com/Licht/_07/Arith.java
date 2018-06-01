d9b5fe8511cdff803858fd24a9d8f7b1bc7bc943
LichtMiao <2804545567@qq.com>
2018-06-01 22:59:55 +0800

    BigDecimal.valueOf()静态方法的使用

--- src/com/Licht/_07/Arith.java
+++ src/com/Licht/_07/Arith.java
@@ -1,3 +1,7 @@
+package com.Licht._07;
+
+import java.math.BigDecimal;
+
 /*
 **定义一个BigDecimal类的工具类Arith类，对double浮点数进行基本运算
 */
@@ -8,39 +12,39 @@ public class Arith{
 	private Arith(){}
 	//提供精确的加法运算
 	public static double add(double v1, double v2){
-		BigDecimal b1 = new BigDecimal.valueOf(v1);
-		BigDecimal b2 = new BigDecimal.valueOf(v2);
+		BigDecimal b1 = BigDecimal.valueOf(v1);
+		BigDecimal b2 = BigDecimal.valueOf(v2);
 		return b1.add(b2).doubleValue();
 	}
 	//精确的减法运算
 	public static double sub(double v1, double v2){
-		BigDecimal b1 = new BigDecimal.valueOf(v1);
-		BigDecimal b2 = new BigDecimal.valueOf(v2);
+		BigDecimal b1 = BigDecimal.valueOf(v1);
+		BigDecimal b2 = BigDecimal.valueOf(v2);
 		return b1.subtract(b2).doubleValue();
 	}
 	//精确的乘法运算
 	public static double mul(double v1, double v2){
-		BigDecimal b1 = new BigDecimal.valueOf(v1);
-		BigDecimal b2 = new BigDecimal.valueOf(v2);
+		BigDecimal b1 = BigDecimal.valueOf(v1);
+		BigDecimal b2 = BigDecimal.valueOf(v2);
 		return b1.multiply(b2).doubleValue();
 	}
 	//（相对）精确的除法运算，当发生除不尽的情况时，
 	//精确到小数点以后10位的数字四舍五入
 	public static double div(double v1, double v2){
-		BigDecimal b1 = new BigDecimal.valueOf(v1);
-		BigDecimal b2 = new BigDecimal.valueOf(v2);
+		BigDecimal b1 = BigDecimal.valueOf(v1);
+		BigDecimal b2 = BigDecimal.valueOf(v2);
 		return b1.divide(b2, DEF_DIV_SCALE
 			, BigDecimal.ROUND_HALF_UP).doubleValue();
 	}
 	public static void main(String[] args){
 		System.out.println("0.05 + 0.01 = " 
-			+ Arith.add(0.05, 0.01);
+			+ Arith.add(0.05, 0.01));
 		System.out.println("1.0 - 0.42 = " 
-			+ Arith.sub(1.0, 0.42);
+			+ Arith.sub(1.0, 0.42));
 		System.out.println("4.015 * 100 = " 
-			+ Arith.mul(4.015, 100);
+			+ Arith.mul(4.015, 100));
 		System.out.println("123.3 / 100 = " 
-			+ Arith.div(123.3, 100);
+			+ Arith.div(123.3, 100));
 	}
 	
 }
