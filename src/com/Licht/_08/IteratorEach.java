eed76b1370b4e120e91f22ec950ed2cdfd26c7a4
LichtMiao <2804545567@qq.com>
2018-06-07 17:52:56 +0800

    使用Lamda表达式遍历Iterator

---
+++ src/com/Licht/_08/IteratorEach.java
@@ -0,0 +1,18 @@
+
+package com.Licht._08;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+public class IteratorEach{
+	public static void main(String[] args){
+		Collection books = new HashSet();
+		books.add("a");
+		books.add("b");
+		books.add("c");
+		//获取books集合对应的迭代器
+		Iterator it = books.iterator();
+		//使用Lamda表达式（目标类型是Consumer)来遍历集合元素
+		it.forEachRemaining(obj -> System.out.println("books集合的元素：" + obj)); 
+	}
+}
