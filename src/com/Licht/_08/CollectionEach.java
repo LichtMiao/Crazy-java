acd384fb92d7167d32305860498efa8d156ac31a
LichtMiao <2804545567@qq.com>
2018-06-07 16:32:46 +0800

    利用Iterable接口的forEach()方法遍历集合元素

---
+++ src/com/Licht/_08/CollectionEach.java
@@ -0,0 +1,15 @@
+package com.Licht._08;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class CollectionEach{
+	public static void main(String[] args){
+		Collection books = new HashSet();
+		books.add("a");
+		books.add("b");
+		books.add("c");
+		//调用Iterable接口的forEach()方法遍历集合
+		books.forEach(obj -> System.out.println("迭代元素集合：" + obj));
+	}
+}
