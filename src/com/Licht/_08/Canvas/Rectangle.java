ce03062f4337b76d5cd118e40739efa4458372c8
LichtMiao <2804545567@qq.com>
2018-06-16 11:35:24 +0800

    delete Canvas

--- src/com/Licht/_08/Canvas/Rectangle.java
+++
@@ -1,7 +0,0 @@
-package com.Licht._08.Canvas;
-//定义Shape的子类Rectangle
-public abstract class Rectangle extends Shape{
-	public void draw(Canvas c){
-		System.out.println("在画布" + c + "上画一个矩形");
-	}
-}
--- src/com/Licht/_08/Canvas/Shape.java
+++
@@ -1,5 +0,0 @@
-package com.Licht._08.Canvas;
-//定义一个抽象类Shape
-public abstract class Shape{
-	public abstract void draw(Canvas c);
-}
