44779f6e04919fb304343fee5d33bcb7ed9114ea
LichtMiao <2804545567@qq.com>
2018-06-29 20:15:47 +0800

    删除多余package

--- src/com/Licht/_16/UserInfoServerClient/ClientThread.java
+++ src/com/Licht/_16/UserInfoServerClient/ClientThread.java
@@ -1,9 +1,6 @@
 package com.Licht._16.UserInfoServerClient;
 
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 /*
